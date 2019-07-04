059810c2d95c264aace0814de88b131e3cf0150b
unknown <cdcdce>
2019-07-04 10:43:26 +0530

    Updates

---
+++ precache-manifest.bfe439710ef1746b3e7b2797195773e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5714b80c76183ad3f14348797da2d2a9",
+    "url": "/wiliyam.github.io/index.html"
+  },
+  {
+    "revision": "ad35986f43bed63d86b5",
+    "url": "/wiliyam.github.io/static/css/main.12c80133.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/wiliyam.github.io/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "ad35986f43bed63d86b5",
+    "url": "/wiliyam.github.io/static/js/main.dc322df7.chunk.js"
+  },
+  {
+    "revision": "6f02ec7eb0a54e2e5e89",
+    "url": "/wiliyam.github.io/static/js/runtime~main.33fa0a02.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wiliyam.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/wiliyam/wiliyam.github.io/precache-manifest.636ffd189dd6807128dd52c03d671a4f.js"
+  "/wiliyam.github.io/precache-manifest.bfe439710ef1746b3e7b2797195773e9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wiliyam/wiliyam.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wiliyam.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
